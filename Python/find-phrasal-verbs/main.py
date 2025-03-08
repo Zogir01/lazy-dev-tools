d0c4bb6ff68d87cf6d834af9147b505b5cf0fc04
Tomasz Wojtasek <tom3k2002w@gmail.com>
2025-03-08 20:25:20 +0100

    change find-phrasal-verbs

--- Python/find-phrasal-verbs/main.py
+++ Python/find-phrasal-verbs/main.py
@@ -1,8 +1,5 @@
-# zrobić możliwośc przekazania kolejnych argumentów jako pliki .pdf lub przekazanie pliku tekstowego z ścieżkami do plików .pdf
-
 import pypdfium2 as pdfium
 import urllib.request
-import urllib.response
 import json
 import sys
 import os
@@ -12,6 +9,7 @@ def main():
     matches = dict()
     arg_cnt = len(sys.argv)
 
+    # process arguments
     for i in range(1, arg_cnt):
         arg = sys.argv[i]
 
@@ -25,50 +23,41 @@ def main():
     if len(pdf_paths) == 0:
         print(f'run script with additional arguments (i.e. paths to pdf files)')
         return
+    
+    # open http connetion to remote file with phrasal verbs
+    url = "https://raw.githubusercontent.com/Zogir01/generated-english-phrasal-verbs/master/phrasal.verbs.build.json"
+    response = urllib.request.urlopen(url)
 
-    with open("results.txt", "w", encoding="utf-8") as result_file:
-        for pdf_path in pdf_paths:
-            pdf = pdfium.PdfDocument(pdf_path)
+    # load json from http response. This object is dictionary 
+    # with keys representing phrasal verbs
+    json_file = json.loads(response.read()) 
 
-            # open http connetion to remote file with phrasal verbs
-            url = "https://raw.githubusercontent.com/Zogir01/generated-english-phrasal-verbs/master/phrasal.verbs.build.json"
-            response = urllib.request.urlopen(url)
+    for pdf_path in pdf_paths:
+        pdf = pdfium.PdfDocument(pdf_path)
 
-            # load json from http response
-            json_file = json.loads(response.read()) 
+        for page in pdf:
+            # convert pdf page to text
+            textpage = page.get_textpage()
 
-            # loaded json is dictionary, with keys representing phrasal verbs
             for verb in json_file.keys():
+                # start searching process
+                searcher = textpage.search(verb, match_case=False, match_whole_word=True)
 
-                # initialize dictionary matches with key = phrasal verb, value = initial count (0)
-                matches.update({verb : 0})
+                # count as long as it searches
+                while searcher.get_next() is not None:
+                    # checks if the verb exists in the dictionary. 
+                    # If it does not, it adds it with a value of 0.
+                    matches.setdefault(verb, 0)
 
-            for page in pdf:
+                    # increment word counter
+                    matches[verb] += 1
 
-                # convert pdf page to text
-                textpage = page.get_textpage()
+    # json serialization
+    json_object = json.dumps(obj = matches, indent = 4)
 
-                for word in matches.keys():
-
-                    # start searching process
-                    searcher = textpage.search(word, match_case=False, match_whole_word=True)
-
-                    # count as long as it searches
-                    while searcher.get_next() is not None:
-                        matches[word] += 1
-
-            # save results
-            #
-            result_file.write(f"path: {pdf_path}, pages: {len(pdf)}, version: {pdf.get_version()}")
-            result_file.write("\n------------------------------------\n")
-
-            for key, value in matches.items():
-
-                # skip those that did not occur
-                if value != 0:
-                    result_file.write(f"{key}: {value}\n")
-
-            result_file.write("\n------------------------------------\n")
+    # save json object as file
+    with open("result.json", "w") as outfile:
+        outfile.write(json_object)
 
 if __name__ == '__main__':
     main()
