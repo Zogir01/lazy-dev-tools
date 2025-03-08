65f7a7b5844633343ca0f894a857dfac82223246
Tomasz Wojtasek <tom3k2002w@gmail.com>
2025-03-08 20:37:41 +0100

    change find-phrasal-verbs

--- Python/find-phrasal-verbs/main.py
+++ Python/find-phrasal-verbs/main.py
@@ -3,30 +3,37 @@ import urllib.request
 import json
 import sys
 import os
+from collections import defaultdict
+
+PHRASAL_VERBS_URL = "https://raw.githubusercontent.com/Zogir01/generated-english-phrasal-verbs/master/phrasal.verbs.build.json"
+RESULT_FILE = "result.json"
 
 def main():
     pdf_paths= list()
-    matches = dict()
+    matches = defaultdict(int)
     arg_cnt = len(sys.argv)
 
     # process arguments
     for i in range(1, arg_cnt):
         arg = sys.argv[i]
 
+        if len(sys.argv < 2):
+            print("Provide at least one PDF file as an argument.")
+            return
+
         # split text into filename (index = 0) , filetype (index = 1) 
         # and check is filetype correct
-        if os.path.splitext(arg)[1] != '.pdf':
-            print(f'invalid argument: {arg}')
+        if os.path.isfile() and os.path.splitext(arg)[1] != '.pdf':
+            print(f'Invalid argument: {arg}')
         else:
             pdf_paths.append(arg)
 
-    if len(pdf_paths) == 0:
-        print(f'run script with additional arguments (i.e. paths to pdf files)')
+    if not pdf_paths:
+        print(f'No valid PDF files found.')
         return
     
     # open http connetion to remote file with phrasal verbs
-    url = "https://raw.githubusercontent.com/Zogir01/generated-english-phrasal-verbs/master/phrasal.verbs.build.json"
-    response = urllib.request.urlopen(url)
+    response = urllib.request.urlopen(PHRASAL_VERBS_URL)
 
     # load json from http response. This object is dictionary 
     # with keys representing phrasal verbs
@@ -45,10 +52,6 @@ def main():
 
                 # count as long as it searches
                 while searcher.get_next() is not None:
-                    # checks if the verb exists in the dictionary. 
-                    # If it does not, it adds it with a value of 0.
-                    matches.setdefault(verb, 0)
-
                     # increment word counter
                     matches[verb] += 1
 
@@ -56,7 +59,7 @@ def main():
     json_object = json.dumps(obj = matches, indent = 4)
 
     # save json object to file
-    with open("result.json", "w") as outfile:
+    with open(RESULT_FILE, "w") as outfile:
         outfile.write(json_object)
 
 if __name__ == '__main__':
